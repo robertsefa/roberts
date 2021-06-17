b580adbe33d199d7b9cf6108fb8c36151de0dca8
robertsefa <80292876+robertsefa@users.noreply.github.com>
2021-06-18 09:56:38 +1200

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,15 @@
+print("What year did Micheal Jordan get drafter?")
+user_response = input()
+
+if user_answer == "1984":
+  print("Correct!")
+  score = score + 1
+
+elif user_answer == "1985":
+  print("Very  Close!")
+
+elif user_answer == "1983":
+  print("Very  Close!")
+
+else:
+  print("incorrect! sad that you dont know")
